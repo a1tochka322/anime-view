12806404e6c21fab82a80eedf754d1e13e9a2ab2
a1tochka322 <a1tochka322@gmail.com>
2022-09-26 04:16:10 +0400

    pre-release

---
+++ js/mainData.js
@@ -0,0 +1,44 @@
+const mainData = () => {
+    const renderAnimeList = (array, genres) => {
+	};
+
+    const renderTopAnime = (array) => {
+        const wrapper = document.querySelector('.filter__gallery');
+        wrapper.innerHTML = '';
+        array.forEach((item) => {
+            wrapper.insertAdjacentHTML(
+                'beforeend',
+                `
+				<div class="product__sidebar__view__item set-bg mix" data-setbg="${item.image}">
+							<div class="ep">${item.rating} / 10</div>
+							<div class="view"><i class="fa fa-eye"></i> ${item.views}</div>
+							<h5><a href="/anime-details.html">${item.title}</a></h5>
+						</div>
+				`
+            );
+        });
+
+        wrapper.querySelectorAll('.set-bg').forEach((elem) => {
+            elem.style.backgroundImage = `url(${elem.dataset.setbg})`;
+        });
+    };
+
+    fetch(
+        'https://anime-622bc-default-rtdb.europe-west1.firebasedatabase.app/anime.json'
+    )
+        .then((response) => {
+            return response.json();
+        })
+        .then((data) => {
+            const genres = new Set();
+
+            renderTopAnime(data.sort((a, b) => b.views - a.views).slice(0, 5));
+            data.forEach((item) => {
+                genres.add(item.ganre);
+            });
+
+			renderAnimeList(data, genres);
+        });
+};
+
+mainData();
